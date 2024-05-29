eb0694ccf6150b1fd0ef63e1cfb5f689e435b568
puvin-2k <sengravin7@gmail.com>
2024-05-29 23:26:09 +0700

    Generate HTML to Summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity:'2'
+},{
+    productId:'dd82ca78-a18b-4e2a-9250-31e67412f98d',
+    quantity:'1'
+}];
 
 export function addToCart(productId){
     let matchingItem;
