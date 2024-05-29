9421a3224ec45366bd35a4b3ac3c80abd9f9db4b
puvin-2k <sengravin7@gmail.com>
2024-05-29 22:13:00 +0700

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,6 @@
-const product = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    price: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    price: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    price: 799,
-  },
-];
+let productHTML = "";
 
-let productHTML = '';
-
-product.forEach((product) => {
-
-    
+products.forEach((product) => {
   productHTML += `
     <div class="product-container">
     <div class="product-image-container">
@@ -46,14 +14,14 @@ product.forEach((product) => {
 
     <div class="product-rating-container">
       <img class="product-rating-stars"
-        src="images/ratings/rating-${product.rating.stars*10}.png">
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
       <div class="product-rating-count link-primary">
         ${product.rating.count}
       </div>
     </div>
 
     <div class="product-price">
-      $${(product.price/100).toFixed(2)}
+      $${(product.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -86,4 +54,4 @@ product.forEach((product) => {
 });
 console.log(productHTML);
 
-document.querySelector('.js-product-grid').innerHTML = productHTML;
+document.querySelector(".js-product-grid").innerHTML = productHTML;
